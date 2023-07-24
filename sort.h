44c376050223bc7459256e4f631b1d6ea485c3f6
Lionel <ndabagajeanlionel@gmail.com>
2023-07-24 13:57:52 +0000

    Added all prototypes in sort.h

--- sort.h
+++ sort.h
@@ -15,9 +15,9 @@
  */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
@@ -26,5 +26,15 @@ void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void swap(int *a, int *b);
 void insertion_sort_list(listint_t **list);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void sort_deck(deck_node_t **deck);
 
 #endif
