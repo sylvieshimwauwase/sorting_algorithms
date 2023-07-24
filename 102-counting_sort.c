1d01f0dfcdac6f2f082d6dbd69127384e8bd7f18
Lionel <ndabagajeanlionel@gmail.com>
2023-07-24 19:37:12 +0000

    initial state of counting sort

---
+++ 102-counting_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * counting_sort - sorts an array of integers in ascending
+ * order using the Counting sort algorithm
+ * @array - The array to be sorted
+ * @size - The length of @array
+ *
+ * Return: void
+ */
+void counting_sort(int *array, size_t size)
+{
+}
