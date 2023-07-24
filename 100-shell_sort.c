b6f73a4fc8e69b655cf546cf489de573ad3709a5
Lionel <ndabagajeanlionel@gmail.com>
2023-07-24 19:06:18 +0000

    Solved betty errors in 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -9,27 +9,28 @@
  */
 void shell_sort(int *array, size_t size)
 {
-    size_t gap, a, b;
+	size_t gap, a, b;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (gap = 1; gap < (size / 3);)
-        gap = gap * 3 + 1;
+	for (gap = 1; gap < (size / 3);)
+		gap = gap * 3 + 1;
 
-    for (; gap >= 1; gap /= 3)
-    {
-        for (a = gap; a < size; a++)
-        {
-            b = a;
-            while (b >= gap && array[b - gap] > array[b])
-            {
-                int swap = array[b];
-                array[b] = array[b - gap];
-                array[b - gap] = swap;
-                b -= gap;
-            }
-        }
-        print_array(array, size);
-    }
+	for (; gap >= 1; gap /= 3)
+	{
+		for (a = gap; a < size; a++)
+		{
+			b = a;
+			while (b >= gap && array[b - gap] > array[b])
+			{
+				int swap = array[b];
+
+				array[b] = array[b - gap];
+				array[b - gap] = swap;
+				b -= gap;
+			}
+		}
+		print_array(array, size);
+	}
 }
