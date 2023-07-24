8f582df4b6e5407c82918d809f7cffd782118bed
sylvieshimwauwase <slvshimwa@gmail.com>
2023-07-24 13:47:58 +0200

    Sorting Algorithms and Big O

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,7 +5,6 @@
  * @a: pointer to first integer
  * @b: pointer to second integer
  */
-/**
 void swap(int *a, int *b)
 {
 	int temp;
@@ -13,7 +12,7 @@ void swap(int *a, int *b)
 	temp = *a;
 	*a = *b;
 	*b = temp;
-}*/
+}
 
 /**
  * bubble_sort - bubble sort algorithm
@@ -22,27 +21,26 @@ void swap(int *a, int *b)
  */
 void bubble_sort(int *array, size_t size)
 {
-	int swapped, temp;
-	size_t i, j;
+	int swapped;
+	size_t i, l = size;
 
 	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	while (1)
 	{
+
 		swapped = 0;
-		for (j = 0; j < size - i - 1; j++)
+		for (i = 0; i < l - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[i] > array[i + 1])
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				swap(&array[i], &array[i + 1]);
 				swapped = 1;
+				print_array(array, size);
 			}
 		}
-		/**printf("%ld", i + 1);*/
-		print_array(array, size);
+		l--;
 
 		if (swapped == 0)
 			break;
