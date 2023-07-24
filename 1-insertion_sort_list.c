938ffd925e47a95176776cbf0edeb6500974cfd7
Lionel <ndabagajeanlionel@gmail.com>
2023-07-24 12:51:16 +0000

    The Insertion sort is working well

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list using insertion sort
+ * @list: Pointer to a pointer to the head of the list
+ *
+ * Description:
+ *     Prints the list after each swap
+ *     Uses the insertion sort algorithm
+ *     Only swaps nodes (including their pointers) and their values
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *iterator, *insert, *tmp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	for (iterator = (*list)->next; iterator != NULL; iterator = tmp)
+	{
+		tmp = iterator->next;
+		insert = iterator->prev;
+
+		while (insert != NULL && iterator->n < insert->n)
+		{
+			if (iterator->next != NULL)
+				iterator->next->prev = insert;
+			insert->next = iterator->next;
+
+			iterator->next = insert;
+			iterator->prev = insert->prev;
+
+			if (insert->prev != NULL)
+				insert->prev->next = iterator;
+			else
+				*list = iterator;
+			insert->prev = iterator;
+
+			insert = iterator->prev;
+
+			/* Print the list after the swap */
+			print_list((const listint_t *)*list);
+		}
+	}
+}
